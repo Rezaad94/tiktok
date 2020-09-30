5916c019c473f8b91f0f2e836ee5c55ddee3178f
RyanCho <chowin21@gmail.com>
2020-09-30 17:42:14 +0900

    update: debug mode

--- desirecaps.py
+++ desirecaps.py
@@ -1,9 +1,22 @@
-desired_caps = {
-  "platformName": "Android",
-  "platformVersion": "6.0",
-  "deviceName": "29aad0ed", 
-  # "deviceName": "192.168.56.103:5555",
-  "appPackage": "com.ss.android.ugc.trill",
-  "app": "D:\\Reza\\tiktok-asia-17-6-3.apk",
-  "appActivity":"com.ss.android.ugc.aweme.splash.SplashActivity"
-}
+DEBUG=False
+
+if DEBUG:
+	desired_caps = {
+	  "platformName": "Android",
+	  "platformVersion": "6.0",
+	  "deviceName": "29aad0ed", 
+	  # "deviceName": "192.168.56.103:5555",
+	  "appPackage": "com.ss.android.ugc.trill",
+	  "app": "D:\\Reza\\tiktok-asia-17-6-3.apk",
+	  "appActivity":"com.ss.android.ugc.aweme.splash.SplashActivity"
+	}
+else:
+	desired_caps = {
+	  "platformName": "Android",
+	  "platformVersion": "10",
+	  "deviceName": "5d71e577", # this for real device
+	  #"deviceName": "192.168.56.103:5555",
+	  "appPackage": "com.ss.android.ugc.trill",
+	  "app": "c:/work/tiktok/tiktok-asia-17-6-41.apk",
+	  "appActivity":"com.ss.android.ugc.aweme.splash.SplashActivity"
+	}
