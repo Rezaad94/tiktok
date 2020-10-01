d919eb8d953961e60f57c754006f7740d5e15c2e
RyanCho <chowin21@gmail.com>
2020-10-01 16:43:32 +0900

    update: id pass

--- scenario1.py
+++ scenario1.py
@@ -15,7 +15,7 @@ if DEBUG:
 	username= 'testersky219@gmail.com'
 	password='tidakada1'
 else:
-	username= 'catwillwork'
+	username= 'catwillwork@gmail.com'
 	password='whenyousleep!@#'
 
 keyword=['청소신','깔끔대장','denps','비타민','곰팡이제거제','생활공작소','윤주부','YJB','오늘습관','하수구','냄새','BLACK','HOLE','거치대','일상공감','브랜드리스','침대의진실','커먼하우스','만능혀클리너','궁서체','만능청소부','두피쿨리닉','LIFE GOODS','곰팡이','세탁기','청소','해충차단','세탁조크리너','깨끗한가','오늘의','발견','크릴오일','화장실','청소','뒤꿈치','각질','레알캠','Real Cam','WHIA휘아','쪽쪽이살균기','라이프굿즈','마사지']
